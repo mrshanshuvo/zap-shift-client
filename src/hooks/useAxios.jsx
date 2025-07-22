f9ace24baa5542460f4c89ec141e4a35b1603762
Shahid Hasan Shuvo <mrshanshuvo@gmail.com>
2025-07-23 02:14:40 +0600

    added

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://zap-shift-server-sand.vercel.app",
 });
 
 const useAxios = () => {
